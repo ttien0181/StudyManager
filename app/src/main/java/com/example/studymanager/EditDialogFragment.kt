9435cc48ae27056429304b29e3952966277715dc
Tú Hoàng <ttien@gmail.com>
2025-05-04 02:34:04 +0700

    init 28/5

--- app/src/main/java/com/example/studymanager/EditDialogFragment.kt
+++ app/src/main/java/com/example/studymanager/EditDialogFragment.kt
@@ -8,16 +8,7 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [EditDialogFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class EditDialogFragment : DialogFragment() {
     private lateinit var hoTenEditText: EditText
     private lateinit var mSSVEditText: EditText
